2f8e725428bd65d8d23c55f36d3fd5eb3eab28f1
ReSin-Yan <a127504443@gmail.com>
2021-07-08 11:51:18 +0800

    Update install.sh

--- k8stools/monitor/kubernetesDashboard/install.sh
+++ k8stools/monitor/kubernetesDashboard/install.sh
@@ -1,15 +1,15 @@
 kubectl apply -f https://raw.githubusercontent.com/kubernetes/dashboard/v2.0.0/aio/deploy/recommended.yaml
 kubectl apply -f admin-user.yaml 
 kubectl apply -f clusterrolebinding.yaml
-echo  '\n'
-echo  'Set tunnel before open browser'
-echo  'Sourceport :           8001' 
-echo  'Destination:           localhost:8001\n'
-echo  'http://localhost:8001/api/v1/namespaces/kubernetes-dashboard/services/https:kubernetes-dashboard:/proxy/\n'
+kubectl dashboardLB.yaml
+#echo  '\n'
+#echo  'Set tunnel before open browser'
+#echo  'Sourceport :           8001' 
+#echo  'Destination:           localhost:8001\n'
+#echo  'http://localhost:8001/api/v1/namespaces/kubernetes-dashboard/services/https:kubernetes-dashboard:/proxy/\n'
 
-echo  'token shouble follow:\n'
+echo  'token Here:\n'
 kubectl -n kubernetes-dashboard get secret $(kubectl -n kubernetes-dashboard get sa/admin-user -o jsonpath="{.secrets[0].name}") -o go-template="{{.data.token | base64decode}}"
-echo  '\n'
+#echo  '\n'
 
-
-kubectl proxy 
+echo  'open on browser and use token to login \n'
